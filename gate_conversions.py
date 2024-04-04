342c27570ea6055bcb49a0e55a908167015315c0
haelyak <ktsai@hmc.edu>
2024-04-04 11:17:45 -0700

    adjusted config files for example board

--- gate_conversions.py
+++ gate_conversions.py
@@ -10,18 +10,27 @@ def segment_to_led(name, dir, segmentnum):
     leds = [] # Array to store leds representing a segment
     
     if "col" == dir: # cols have 3 LEDs per segment
-        previous = sum(LEDconsts.collengths[:name + 1]) # add up the previous cols to figure out starting LED
-        ledstart = previous + segmentnum*3 # each segment has 3LEDs
+        previous = sum(LEDconsts.collengths[:name]) # add up the previous cols to figure out starting LED
+        ledstart = previous + segmentnum*2 # each segment has 3LEDs but overlaps with 1 LED from previous segment
         leds += [ledstart, ledstart+1, ledstart+2]
     if "row" == dir: # rows have 4 LEDs per segment
-        previous = sum(LEDconsts.rowlengths[:name + 1])
+        previous = sum(LEDconsts.collengths) + sum(LEDconsts.rowlengths[:name])
         ledstart = previous + segmentnum*4
         leds += [ledstart, ledstart+1, ledstart+2, ledstart+3]
     return leds
 
-# print(segment_to_led(1, "row", 1))
+segpercol = [2, 3, 4, 5, 6, 6, 6, 5, 4, 3, 2]
+for i in range(0,len(LEDconsts.collengths)):
+    for j in range(0, segpercol[i]):
+        print("(", i, "col", j, ")", segment_to_led(i,"col", j))
 
-# print(segment_to_led(5, "col", 20))
+
+segperrow = [2, 4, 6, 8, 10, 10, 10]
+for i in range(0,len(LEDconsts.rowlengths)):
+    for j in range(0, segperrow[i]):
+        print("(", i, "row", j, ")", segment_to_led(i,"row", j))
+
+
 
 def gate_to_led(gate):
     """ Converts a given gate to specific LED pixels
@@ -34,4 +43,5 @@ def gate_to_led(gate):
         leds+= segment_to_led(seg[0], seg[1], seg[2]) # Segments are arrays with name, dir, segmentnum
     return leds
 
-print(gate_to_led(1))
+for i in range(0, 6):
+    print("gate", i, ":", gate_to_led(i))
