c20fbc50350c2a3acaf5a8ebf2efc3be3a564820
haelyak <ktsai@hmc.edu>
2024-04-28 18:10:56 -0700

    reads gates!

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 import json
 import board
 import adafruit_dotstar as dotstar
-# import RPi.GPIO as GPIO
 import time
 import gate_conversions
 import data_reader
@@ -37,7 +36,6 @@ def setup():
                     dots[LED] = (255, 255, 255)
                     dots.show()
                     time.sleep(0.05)
-                print(inputLEDs)
 
 
 
@@ -54,69 +52,72 @@ def compute_gate(gate, mux_index, is_four_pin):
     gateLEDs = [] # Array to hold the LEDs representing a logic gate
 
     inputs = [] # Convert input segments from json to led pixels
-    for input in data[gate]["input"]:
+    for input in gate[1]["input"]:
         inputs += gate_conversions.segment_to_led(input[0], input[1], input[2])
 
     outputs = [] # Convert output segments from json to led pixels
-    for output in data[gate]["output"]:
+    for output in gate[1]["output"]:
         outputs += gate_conversions.segment_to_led(output[0], output[1], output[2])
 
-    gateType = ["NAND", "AND", "OR", "XOR" ]
+    # gateType = ["NAND", "AND", "OR", "XOR" ]
 
     board_data = data_reader.read_data() # Read in all the data from the sensor boards
 
-    gateType = data_reader.parse_gates(board_data, gate, mux_index, is_four_pin)
+    gateType = data_reader.parse_gates(board_data, int(gate[0]), mux_index, is_four_pin)
     
-    if type in data[gate]["accepts"]:
-        if type == "NAND":
+    print(board_data)
+    print(gateType)
+    
+    if gateType in gate[1]["accepts"]:
+        if gateType == "NAND":
             if all(is_pixel_lit(p) for p in inputs):
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (0, 0, 0) # Turn off perimeters of gates for False
                 return False
             else:
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (255, 0, 0) # Light up the perimeters of the gates in red for True
                 for LED in outputs:
                     dots[LED] = (255, 0, 0) # Light up the output segments in red for True
                 return True
-        elif type == "AND":
+        elif gateType == "AND":
             if all(is_pixel_lit(p) for p in inputs):
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (255, 0, 0) # Light up the perimeters of the gates in red for True
                 for LED in outputs:
                     dots[LED] = (255, 0, 0) # Light up the output segments in red for True
                 return True
             else:
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (0, 0, 0) # Turn off perimeters of gates for False
                 return False
         elif type == "OR":
             if any(is_pixel_lit(p) for p in inputs):
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (255, 0, 0) # Light up the perimeters of the gates in red for True
                 for LED in outputs:
                     dots[LED] = (255, 0, 0) # Light up the output segments in red for True
                 return True
             else:
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (0, 0, 0) # Turn off perimeters of gates for False
                 return False
         elif type == "XOR":
             if sum(is_pixel_lit(p) for p in inputs) % 2 != 0:
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (255, 0, 0) # Light up the perimeters of the gates in red
                 for LED in outputs:
                     dots[LED] = (255, 0, 0) # Light up the output segments
                 return True
             else:
-                gateLEDs += gate_conversions.gate_to_led(gate)
+                gateLEDs += gate_conversions.gate_to_led(int(gate[0]))
                 for LED in gateLEDs:
                     dots[LED] = (0, 0, 0) # Turn off perimeters of gates
                 return False
@@ -124,10 +125,10 @@ def compute_gate(gate, mux_index, is_four_pin):
 def compute_switch(switch, mux_index, is_four_pin):
     board_data = data_reader.read_data() # Read in all the data from the sensor boards
     
-    switchType = data_reader.parse_gates(board_data, switch, mux_index, is_four_pin)
+    switchType = data_reader.parse_gates(board_data, int(switch[0]), mux_index, is_four_pin)
 
-    outputLEDs = [] # Convert input segments from json to led pixels
-    for output in data[switch]["input"]:
+    outputLEDs = [] # Convert output segments from json to led pixels
+    for output in switch[1]["output"]:
         outputLEDs += gate_conversions.segment_to_led(output[0], output[1], output[2])
 
     if switchType == "ON":
@@ -140,10 +141,13 @@ def compute_switch(switch, mux_index, is_four_pin):
 
 
 setup()
+print("Done setup")
 while True:
     for switch in data['switches'].items(): # Loop through all the switches in the puzzle
-        compute_gate(gate, gate["mux_index"], gate["is_four_pin"]) 
+        compute_switch(switch, switch[1]["mux_index"], switch[1]["is_four_pin"]) 
+    print("Done switches")
         
     for gate in data['gates'].items(): # Loop through all the gates in the puzzle
-        compute_gate(gate, gate["mux_index"], gate["is_four_pin"]) 
+        compute_gate(gate, gate[1]["mux_index"], gate[1]["is_four_pin"]) 
+    print("Done gates")
     
---
+++ data_reader.py
@@ -0,0 +1,76 @@
+import board
+import time
+import digitalio
+
+INPUT = digitalio.Direction.INPUT
+OUTPUT = digitalio.Direction.OUTPUT
+
+# data selection pins
+DS_0 = digitalio.DigitalInOut(board.D9)
+DS_0.direction = OUTPUT
+DS_1 = digitalio.DigitalInOut(board.D10)
+DS_1.direction = OUTPUT
+DS_2 = digitalio.DigitalInOut(board.D11)
+DS_2.direction = OUTPUT
+DS_3 = digitalio.DigitalInOut(board.D12)
+DS_3.direction = OUTPUT
+
+#data input pins
+
+DATA_0 = digitalio.DigitalInOut(board.A0)
+DATA_0.direction = INPUT
+DATA_1 = digitalio.DigitalInOut(board.A1)
+DATA_1.direction = INPUT
+
+def read_data():
+    data = [False for i in range(32)]
+    for i in range(16):
+        n = i
+        DS_0.value = bool(n % 2)
+
+        n >>= 1
+        DS_1.value = bool(n % 2)
+
+        n >>= 1
+        DS_2.value = bool(n % 2)
+
+        n >>= 1
+        DS_3.value = bool(n % 2)
+
+        time.sleep(0.001) # wait 1ms
+
+        data[i] = DATA_0.value
+        data[16+i] = DATA_1.value
+
+    return data
+
+
+GATES = {
+    (1,0,0,1): "NAND",
+    (1,0,1,1): "OR",
+    (1,1,0,1): "AND",
+    (1,1,1,1): "XOR",
+    (0,1): "OFF",
+    (1,1): "ON",
+
+    # invalid data because no contact
+    (0,0,0,0): None,
+    (0,0,1,0): None,
+    (0,1,0,0): None,
+    (0,1,1,0): None,
+    (1,0,0,0): None,
+    (1,0,1,0): None,
+    (1,1,0,0): None,
+    (1,1,1,0): None,
+    (0,0,0,1): None,
+    (0,0,1,1): None,
+    (0,1,0,1): None,
+    (0,1,1,1): None,
+    (0,0): None,
+    (1,0): None
+}
+
+def parse_gates(data, gate_index, mux_index, is_four_pin):
+    inc = 4 if is_four_pin else 2
+
+    return GATES[tuple(data[mux_index*16+gate_index*inc:mux_index*16+gate_index*inc+inc] )]
